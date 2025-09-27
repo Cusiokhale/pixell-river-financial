6d8a6fa005c2f3c93b5843f8198e9285ba338e2d
cordelia usiokhale <cordeliausiokhale@academic.rrc.ca>
2025-09-27 16:14:13 -0500

    feat: add support for req.body and import employee routes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import { Server } from "http";
 import express, { Express } from "express";
 import morgan from "morgan";
 
+import employeesRoutes from "./api/v1/routes/employees";
+
 const app: Express = express();
 
 const PORT: number = parseInt(process.env.PORT || '3003');
 
+app.use(express.json());
 app.use(morgan("combined"));
 
 // I had to add an underscore infront of req to stop the TS errors
@@ -16,6 +19,8 @@ app.get("/", (_req, res) => {
 app.get("/health", (_req, res) => {
     res.send("Server is healthy")
 });
+
+app.use("/api/v1/employees", employeesRoutes)
 
 const server: Server = app.listen(PORT, '0.0.0.0', 0, () => {
     console.log(`Server is running on port ${PORT}`);
